4ec6ddcb54f8446e05382bd7ba29f0b2f243497f
Derek Ellis <dkellis42@gmail.com>
2014-08-14 10:00:37 -0500

    added rentSplitter.rb connecting file for relatives

---
+++ lib/rentSplitter.rb
@@ -0,0 +1,10 @@
+module ShowMeMoney
+
+end
+
+require_relative 'sesh/entities/bills.rb'
+require_relative 'sesh/entities/rent.rb'
+require_relative 'sesh/entities/user.rb'
+require_relative 'sesh/databases/dbi.rb'
+require_relative 'sesh/scripts/signin.rb'
+require_relative 'sesh/scripts/signup.rb'
