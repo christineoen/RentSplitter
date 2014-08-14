8096b3c75291a7eed19e6affa967b3fab82f5d16
Derek Ellis <dkellis42@gmail.com>
2014-08-14 09:57:05 -0500

    Added user foundation for ruby classes

---
+++ lib/sesh/entities/user.rb
@@ -0,0 +1,22 @@
+require 'digest/sha1'
+
+module ShowMeMoney
+  class User
+    attr_reader :username, :password_digest, :wins, :losses
+
+    def initialize(username, password_digest=nil)
+      @username = username
+      @password_digest = password_digest
+      @wins = 0
+      @losses = 0
+    end
+
+    def update_password(password)
+      @password_digest = Digest::SHA1.hexdigest(password)
+    end
+
+    def has_password?(password)
+      Digest::SHA1.hexdigest(password) == @password_digest
+    end
+  end
+end
